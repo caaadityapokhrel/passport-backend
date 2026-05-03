4353c5e893c5541fccbbeadcc8c82420de3e96d3
Aaditya Pokhrel <caaadityapokhrel@gmail.com>
2026-05-03 06:30:21 +0545

    fix

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.get('/check-detail', async (req, res) => {
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
-});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
